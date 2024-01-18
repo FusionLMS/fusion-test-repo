f9e6e91b264cb5d761dcdca6a04cf202a40c452c
Bardin08 <vbardin810@gmail.com>
2024-01-18 23:04:59 +0200

    Add test project

---
+++ FusionPlayground/ConsoleApp1.Tests/SimpleCalculatorTests.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1.Tests;
+
+public class SimpleCalculatorTests
+{
+    [Theory]
+    [InlineData(1, 2, 3)]
+    [InlineData(2, 3, 5)]
+    [InlineData(3, 4, 7)]
+    [InlineData(4, 5, 9)]
+    public void Sum_ParamsValid_Success(int a, int b, int expected)
+    {
+        var actual = SimpleCalculator.Sum(a, b);
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ FusionPlayground/ConsoleApp1/SimpleCalculator.cs
@@ -0,0 +1,6 @@
+﻿namespace ConsoleApp1;
+
+public static class SimpleCalculator
+{
+    public static double Sum(int a, int b) => a + b;
+}
